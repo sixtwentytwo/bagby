ba51aca5ec8e71c002a9b7cb0d31102f6ceb1f95
sixtwentytwo <alberstein@gmail.com>
2016-01-03 13:16:45 -0800

    More tweaks

--- woocommerce.php
+++ woocommerce.php
@@ -5,15 +5,17 @@
 <div id="inner-content" class="wrap clearfix">
 <?php if (is_active_sidebar('sidebar2')) : ?>
 <div id="main" class="m-all t-2of3 d-2of3 wrap cf" role="main">
-<?php else: ?>
+<?php else : ?>
 <div id="main" class="m-all t-all d-all wrap cf" role="main">
 <?php endif;?>
 <?php woocommerce_content(); ?>
 
 </div>
-
+<?php if (is_active_sidebar('sidebar1')) : ?>
 <?php get_sidebar(); ?>
-
+<?php else :
+endif ;
+?>
 </div>
 
 </div>
