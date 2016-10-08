be58ee488a4c749052f156f9bc9ff119a71136ac
sixtwentytwo <alberstein@gmail.com>
2016-10-07 19:17:34 -0700

    Merge remote-tracking branch 'eddiemachado/master'
    # Conflicts:
    #	functions.php

--- functions.php
+++ functions.php
@@ -251,12 +251,17 @@ external fonts. If you're using Google Fonts, you
 can replace these fonts, change it in your scss files
 and be up and running in seconds.
 */
+<<<<<<< HEAD
 function bones_fonts()
 {
     wp_enqueue_style(
         'googleFonts',
         'http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic'
     );
+=======
+function bones_fonts() {
+  wp_enqueue_style('googleFonts', '//fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic');
+>>>>>>> eddiemachado/master
 }
 
 add_action('wp_enqueue_scripts', 'bones_fonts');
